45a1e7a54cfa9e5af1b8c05c7689a51d5add56ee
Thomas Ren <thomas@Thomass-MacBook-Pro.local>
2017-01-18 15:05:34 -0800

    Moved array teaching into prog1.c

--- prog0.c
+++ prog0.c
@@ -46,12 +46,6 @@ int main()
 
   /* Write your code for TODO 1 above this line  */
 
-  // Arrays
-  // Almost all the computert programs need to deal with a big chunk of data
-  // An array is a built-in data structure in almost all kinds of programming 
-  // languages, including C. It looks like this:
-  int[] arr = {8, 2, 3, 1, 2};
-  
   // Let's try out more operations!
   double div = c / d;
   int div2 = a / b;
--- prog1.c
+++ prog1.c
@@ -1,14 +1,21 @@
 /*
  * Filename: prog1.c
  * Author: Junru Ren
- * Description: C program for learning learning logics and controls in C
+ * Description: C program for learning array, logics and controls in C
  * Date: Jan 18 2017
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-
+int main()
+{
+  // Arrays
+  // Almost all the computer programs need to deal with big chunks of data
+  // Array is a built-in data structure in C (also in almost all other languages)
+  // It looks like this:
+  int[] arr = {8, 7, 2, 3, 5};
+  // Above is an array of 5 integers; each element is stored next to each others
+  // in the memory
   return EXIT_SUCCESS;
 }
