f2d2aa6ab1c017d5ce25c423e8dd61cdf9e3be6a
Junru Ren <jur009@ieng6-201.ucsd.edu>
2017-01-17 22:19:05 -0800

    Add file header

--- hello.c
+++ hello.c
@@ -1,3 +1,11 @@
+/*
+ * Filename: hello.c
+ * Author: Junru Ren
+ * Description: The very first program for Intro to C Workshop by IEEE, HKN, and
+ *              TBP at UC San Diego
+ * Date: Jan 17 2017
+ */
+
 #include <stdio.h>
 
 int main()
